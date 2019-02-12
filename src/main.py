581e6fc462c1e8f45bc55d9367fc25b4cc6ab7ee
Mikhail Khimukhin <khimukhin@ians.aero>
2019-02-12 10:28:30 +0300

    Loader put to order

--- src/main.py
+++ src/main.py
@@ -1,27 +1,14 @@
 import sys
-import yaml
-from mwidgets import Loader, PygameGUI
+from mwidgets import Loader
 from mainwindow import MainWindow
 from gamescene import GameScene
 
 
 if __name__ == "__main__":
-    # Load config
+    Loader.register_widget("MainWindow", MainWindow)
+    Loader.register_widget("GameScene", GameScene)
     path = sys.argv[1] if len(sys.argv) > 1 else "cfg/menu.yaml"
-    file = open(path, 'r')
-    config = yaml.safe_load(file)
-    file.close()
-    # Initialize gui
-    *_, w, h = config["rect"]
-    gui = PygameGUI(w, h)
-    # Load application
-    loader = Loader()
-    loader.register_widget("MainWindow", MainWindow)
-    loader.register_widget("GameScene", GameScene)
-    main_window = loader.load(config)
-    loader.clean()
-    main_window.set_gui(gui)
-    # Start exec loop
+    main_window = Loader.load_yaml(path)
     main_window.exec()
 
 # TODO Document basic widgets and thier roles
