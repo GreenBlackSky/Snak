0ec96801b2ea97883954e82c5ead837d94f3c15a
Mikhail Khimukhin <khimukhin@ians.aero>
2019-02-26 11:52:36 +0300

    Window layout replaced by widget

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -8,7 +8,9 @@ class MainWindow(Window):
 
     def __init__(self, gui, layout_name=None):
         """Create new MainWindow with name of initial top layout."""
-        super().__init__(gui, layout_name)
+        super().__init__(gui)
+        self.layouts = dict()
+        self.default_layout_name = layout_name
         self.triggers = {
             **self.triggers,
             "pause_game": self.pause_game,
@@ -18,17 +20,29 @@ class MainWindow(Window):
             "start_new_game": self.start_new_game
         }
 
+    @classmethod
+    def from_config(cls, config, gui):
+        """Load window and its heirs from config."""
+        ret = cls(gui, config["layout"])
+        return ret
+
+    def add_child(self, widget, id):
+        """Add child widget."""
+        self.layouts[id] = widget
+        if not self.widget or id == self.default_layout_name:
+            self.set_widget(self.layouts[id])
+
     def pause_game(self):
-        self.set_layout("pause_menu")
+        self.set_widget(self.layouts["pause_menu"])
 
     def continue_game(self):
-        self.set_layout("game_scene")
+        self.set_widget(self.layouts["game_scene"])
 
     def open_main_menu(self):
-        self.set_layout("main_menu")
+        self.set_widget(self.layouts["main_menu"])
 
     def open_evolution_menu(self):
-        self.set_layout("evolution_menu")
+        self.set_widget(self.layouts["evolution_menu"])
 
     def start_new_game(self):
-        self.set_layout("game_scene")
+        self.set_widget(self.layouts["game_scene"])
